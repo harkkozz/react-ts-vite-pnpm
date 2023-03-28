72698879e905f83de68b82ed9ac390ba496b4958
harkkozz <adilovicharis94@gmail.com>
2023-03-28 16:30:21 +0200

    add more config

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
+import checker from 'vite-plugin-checker';
 import svgr from 'vite-plugin-svgr';
 import tsConfigPath from 'vite-tsconfig-paths';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), svgr(), tsConfigPath()],
+  plugins: [react(), svgr(), tsConfigPath(), checker({ typescript: true })],
   server: { port: 3000 }
 });
